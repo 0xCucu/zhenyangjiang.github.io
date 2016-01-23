cd7093a7388968a2f7f7299df60a912a2c4170b9
zhenyangjiang <1061120731@qq.com>
2016-01-23 23:14:23 +0800

    1-23-23:14

---
+++ _site/assets/js/core.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var move = [
+		
+		function(){$(".hover").animate({opacity:'1',top:'0px'},'fast',next)},
+		function(){$(".nav-move").animate({opacity:'1',top:'0px'},'fast',next)},
+		function(){$(".jumb-move").animate({opacity:'1',top:'100px'},next)},
+		function(){$(".jumb-move2").animate({opacity:'1',left:'0px'},1000,next)},
+		function(){$(".home h1").animate({opacity:'1'},1000,next)},
+		function(){$(".post-list li").animate({opacity:'1',top:'0px'},'fast',next)},
+		function(){$(".site-footer").animate({opacity:'1',top:'0px'},'fast')}
+		
+	]
+	next = function(){
+		$(document).dequeue("move");
+	}
+	next();
+	$(document).queue("move",move);	
+	$(".line-before i").animate({left:'0px'},600);
+	$(".line-after i").animate({left:'0px'},600,function(){
+		$(document).dequeue("move");
+	});
+	
+	
+	
+});
